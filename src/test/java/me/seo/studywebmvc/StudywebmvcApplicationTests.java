5d317fc1ca5acdfb2445a5fb889b535314848141
tjwjdgks <tjwjdgks43@gmail.com>
2021-08-30 14:31:47 +0900

    study webmvc

---
+++ src/test/java/me/seo/studywebmvc/StudywebmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seo.studywebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudywebmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
