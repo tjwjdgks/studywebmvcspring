8167ddb55238e4b0401275bfc67dab400c426ef2
tjwjdgks <tjwjdgks43@gmail.com>
2021-09-03 13:13:49 +0900

    test

--- src/main/java/me/seo/studywebmvc/config/WebConfig.java
+++ src/main/java/me/seo/studywebmvc/config/WebConfig.java
@@ -5,6 +5,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+// spring mvc 인터페이스 확장
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
